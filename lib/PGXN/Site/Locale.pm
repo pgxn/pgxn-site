4b3e4c05eb3e01f13b318577ec9d3d9a5ab16b5d
David E. Wheeler <david@justatheory.com>
2011-03-03 15:43:55 -0800

    Copy local support from PGXN::Manager.

---
+++ lib/PGXN/Site/Locale.pm
@@ -0,0 +1,140 @@
+package PGXN::Site::Locale;
+
+use 5.12.0;
+use utf8;
+use parent 'Locale::Maketext';
+use I18N::LangTags::Detect;
+
+# Allow unknown phrases to just pass-through.
+our %Lexicon = (
+    _AUTO => 1,
+    listcomma => ',',
+    listand   => 'and',
+    openquote => '“',
+    shutquote => '”',
+    in        => 'in',
+);
+
+sub accept {
+    shift->get_handle( I18N::LangTags::Detect->http_accept_langs(shift) );
+}
+
+sub list {
+    my ($lh, $items) = @_;
+    return unless @{ $items };
+    return $items->[0] if @{ $items } == 1;
+    my $last = pop @{ $items };
+    my $comma = $lh->maketext('listcomma');
+    my $ret = join  "$comma ", @$items;
+    $ret .= $comma if @{ $items } > 1;
+    my $and = $lh->maketext('listand');
+    return "$ret $and $last";
+}
+
+sub qlist {
+    my ($lh, $items) = @_;
+    return unless @{ $items };
+    my $open = $lh->maketext('openquote');
+    my $shut = $lh->maketext('shutquote');
+    return $open . $items->[0] . $shut if @{ $items } == 1;
+    my $last = pop @{ $items };
+    my $comma = $lh->maketext('listcomma');
+    my $ret = $open . join("$shut$comma $open", @$items) . $shut;
+    $ret .= $comma if @{ $items } > 1;
+    my $and = $lh->maketext('listand');
+    return "$ret $and $open$last$shut";
+}
+
+1;
+
+=head1 Name
+
+PGXN::Site::Locale - Localization for PGXN::Site
+
+=head1 Synopsis
+
+  use PGXN::Site::Locale;
+  my $mt = PGXN::Site::Locale->accept($env->{HTTP_ACCEPT_LANGUAGE});
+
+=head1 Description
+
+This class provides localization support for PGXN::Site. Each locale must
+create a subclass named for the locale and put its translations in the
+C<%Lexicon> hash. It is further designed to support easy creation of
+a handle from an HTTP_ACCEPT_LANGUAGE header.
+
+=head1 Interface
+
+The interface inherits from L<Locale::Maketext> and adds the following
+method.
+
+=head2 Constructor Methods
+
+=head3 C<accept>
+
+  my $mt = PGXN::Site::Locale->accept($env->{HTTP_ACCEPT_LANGUAGE});
+
+Returns a PGXN::Site::Locale handle appropriate for the specified
+argument, which must take the form of the HTTP_ACCEPT_LANGUAGE string
+typically created in web server environments and specified in L<RFC
+3282|http://tools.ietf.org/html/rfc3282>. The parsing of this header is
+handled by L<I18N::LangTags::Detect>.
+
+=head2 Instance Methods
+
+=head3 C<list>
+
+  # "Missing these keys: foo, bar, and baz"
+  say $mt->maketext(
+      'Missing these keys: [list,_1])'
+      [qw(foo bar baz)],
+  );
+
+Formats a list of items. The list of items to be formatted should be passed as
+an array reference. If there is only one item, it will be returned. If there
+are two, they will be joined with " and ". If there are more, there will be a
+comma-separated list with the final item joined on ", and ".
+
+Note that locales can control the localization of the comma and "and" via the
+C<listcomma> and C<listand> entries in their C<%Lexicon>s.
+
+=head3 C<qlist>
+
+  # "Missing these keys: “foo”, “bar”, and “baz”
+  say $mt->maketext(
+      'Missing these keys: [qlist,_1]'
+      [qw(foo bar baz)],
+  );
+
+Like C<list()> but quotes each item in the list. Locales can specify the
+quotation characters to be used via the C<openquote> and C<shutquote> entries
+in their C<%Lexicon>s.
+
+head1 Author
+
+David E. Wheeler <david.wheeler@pgexperts.com>
+
+=head1 Copyright and License
+
+Copyright (c) 2010-2011 David E. Wheeler.
+
+This module is free software; you can redistribute it and/or modify it under
+the L<PostgreSQL License|http://www.opensource.org/licenses/postgresql>.
+
+Permission to use, copy, modify, and distribute this software and its
+documentation for any purpose, without fee, and without a written agreement is
+hereby granted, provided that the above copyright notice and this paragraph
+and the following two paragraphs appear in all copies.
+
+In no event shall David E. Wheeler be liable to any party for direct,
+indirect, special, incidental, or consequential damages, including lost
+profits, arising out of the use of this software and its documentation, even
+if David E. Wheeler has been advised of the possibility of such damage.
+
+David E. Wheeler specifically disclaims any warranties, including, but not
+limited to, the implied warranties of merchantability and fitness for a
+particular purpose. The software provided hereunder is on an "as is" basis,
+and David E. Wheeler has no obligations to provide maintenance, support,
+updates, enhancements, or modifications.
+
+=cut
---
+++ t/locale.t
@@ -0,0 +1,70 @@
+#!/usr/bin/env perl -w
+
+use 5.12.0;
+use utf8;
+use Test::More tests => 29;
+#use Test::More 'no_plan';
+my $CLASS;
+
+BEGIN {
+    $CLASS = 'PGXN::Site::Locale';
+    use_ok $CLASS or die;
+}
+
+isa_ok my $l = $CLASS->get_handle('en'), $CLASS, 'English handle';
+isa_ok $l, "$CLASS\::en", 'It also';
+is $l->maketext('Welcome'), 'Welcome', 'It should translate "Welcome"';
+
+# Try get_handle() with bogus language.
+isa_ok $l = $CLASS->get_handle('nonesuch'), $CLASS, 'Nonesuch get_handle handle';
+isa_ok $l, "$CLASS\::en", 'It also';
+is $l->maketext('Welcome'), 'Welcome', 'It should translate "Welcome"';
+
+# Try french.
+isa_ok $l = $CLASS->get_handle('fr'), $CLASS, 'French handle';
+isa_ok $l, "$CLASS\::fr", 'It also';
+is $l->maketext('in'), 'dans', 'It should translate "in"';
+
+# Try pass-through.
+is $l->maketext('whatever'), 'whatever', 'It should pass through unknown phrase';
+
+# Try accept.
+isa_ok $l = $CLASS->accept('en;q=1,fr;q=.5'), $CLASS, 'Accept handle';
+isa_ok $l, "$CLASS\::en", 'It also';
+
+isa_ok $l = $CLASS->accept('en;q=1,fr;q=2'), $CLASS, 'French accept handle';
+isa_ok $l, "$CLASS\::fr", 'It also';
+
+# Try en list().
+ok my $lh = $CLASS->get_handle('en'), 'Get English handle';
+is $lh->maketext('[list,_1]', ['foo', 'bar']),
+    'foo and bar', 'en list() should work';
+is $lh->maketext('[list,_1]', ['foo']),
+    'foo', 'single-item en list() should work';
+is $lh->maketext('[list,_1]', ['foo', 'bar', 'baz']),
+    'foo, bar, and baz', 'triple-item en list() should work';
+
+# Try en qlist()
+is $lh->maketext('[qlist,_1]', ['foo', 'bar']),
+    '“foo” and “bar”', 'en qlist() should work';
+is $lh->maketext('[qlist,_1]', ['foo']),
+    '“foo”', 'single-item en qlist() should work';
+is $lh->maketext('[qlist,_1]', ['foo', 'bar', 'baz']),
+    '“foo”, “bar”, and “baz”', 'triple-item en qlist() should work';
+
+# Try fr list().
+ok $lh = $CLASS->get_handle('fr'), 'Get Frglish hetle';
+is $lh->maketext('[list,_1]', ['foo', 'bar']),
+    'foo et bar', 'fr list() should work';
+is $lh->maketext('[list,_1]', ['foo']),
+    'foo', 'single-item fr list() should work';
+is $lh->maketext('[list,_1]', ['foo', 'bar', 'baz']),
+    'foo, bar, et baz', 'triple-item fr list() should work';
+
+# Try fr qlist()
+is $lh->maketext('[qlist,_1]', ['foo', 'bar']),
+    '«foo» et «bar»', 'fr qlist() should work';
+is $lh->maketext('[qlist,_1]', ['foo']),
+    '«foo»', 'single-item fr qlist() should work';
+is $lh->maketext('[qlist,_1]', ['foo', 'bar', 'baz']),
+    '«foo», «bar», et «baz»', 'triple-item fr qlist() should work';
