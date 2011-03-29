db256e7ed2144c418d7b917b701b87978edd7f84
David E. Wheeler <david@justatheory.com>
2011-03-28 22:41:07 -0400

    Move www/ui to just ui.

--- lib/PGXN/Site/Router.pm
+++ lib/PGXN/Site/Router.pm
@@ -10,7 +10,7 @@ use Plack::App::File;
 sub app {
     my $class = shift;
     my $controller = PGXN::Site::Controller->new(@_);
-    my $files      = Plack::App::File->new(root => './www/ui/');
+    my $files      = Plack::App::File->new(root => './ui/');
     my $router = router {
         missing { $controller->missing(@_) };
 
