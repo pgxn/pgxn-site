eea0296493d6308f0c7a6764364aa12b70575d61
David E. Wheeler <david@justatheory.com>
2011-04-29 14:55:25 -0700

    Increment to v0.6.5.

--- lib/PGXN/Site/Locale/en.pm
+++ lib/PGXN/Site/Locale/en.pm
@@ -1,4 +1,4 @@
-package PGXN::Site::Locale::en v0.6.4;
+package PGXN::Site::Locale::en v0.6.5;
 
 use 5.12.0;
 use utf8;
--- lib/PGXN/Site/Templates.pm
+++ lib/PGXN/Site/Templates.pm
@@ -1,4 +1,4 @@
-package PGXN::Site::Templates v0.6.4;
+package PGXN::Site::Templates v0.6.5;
 
 use 5.12.0;
 use utf8;
