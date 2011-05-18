pgxn/pgxn-site

commit 9e77c26c67708ae44fa4811f31af44686aa6f5d1

Fix dt wrapping issue in Firefox on Ubuntu.

lib/PGXN/Site/ui/css/layout.css
@@ -549,6 +549,7 @@ ul.sideMenu {
    text-align:right;
    width: 8em;
    float:left;
+   clear:both; /* Needed for some versions of FireFox */
 }
 
 .dist .meta dt:after {

commit 6d1eeee26389b61fc82afaa7b1af299d4397728f

Fix alignment of `dt`s on user page.
Was broken in 9e77c26c67708ae44fa4811f31af44686aa6f5d1. Fix by changing the
`clear:both` added in that commit to apply *only* to the label used
for the "Other Releases" select list. Closes #1.

lib/PGXN/Site/Templates.pm
@@ -330,7 +330,7 @@ template distribution => sub {
                             }
                         }
                         # Create a select list of all other versions.
-                        dt { T 'Other Releases' };
+                        dt { class is 'other'; T 'Other Releases' };
                         dd {
                             select {
                                 onchange is 'window.location.href = this.options[this.selectedIndex].value';

lib/PGXN/Site/ui/css/layout.css
@@ -549,6 +549,9 @@ ul.sideMenu {
    text-align:right;
    width: 8em;
    float:left;
+}
+
+.dist .meta dt.other {
    clear:both; /* Needed for some versions of FireFox */
 }
 