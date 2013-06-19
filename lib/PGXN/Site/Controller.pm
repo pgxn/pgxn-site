654f4cdac32f9d6a03f0a9a19ec87c04cdd72d8c
David E. Wheeler <david@justatheory.com>
2013-06-18 23:36:28 -0700

    Increment to v0.10.3.

--- lib/PGXN/Site/Controller.pm
+++ lib/PGXN/Site/Controller.pm
@@ -12,7 +12,7 @@ use Encode;
 use WWW::PGXN;
 use List::MoreUtils qw(any);
 use namespace::autoclean;
-our $VERSION = v0.10.2;
+our $VERSION = v0.10.3;
 
 Template::Declare->init( dispatch_to => ['PGXN::Site::Templates'] );
 
