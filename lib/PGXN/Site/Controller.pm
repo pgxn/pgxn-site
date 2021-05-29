0548f2e87def87facb1f7846173e2d851e1152a8
David E. Wheeler <david@justatheory.com>
2021-05-29 17:16:24 -0400

    Increment to v0.22.0

--- lib/PGXN/Site/Controller.pm
+++ lib/PGXN/Site/Controller.pm
@@ -11,7 +11,7 @@ use Encode;
 use WWW::PGXN;
 use List::MoreUtils qw(any);
 use namespace::autoclean;
-our $VERSION = v0.21.1;
+our $VERSION = v0.22.0;
 
 Template::Declare->init( dispatch_to => ['PGXN::Site::Templates'] );
 
--- lib/PGXN/Site/Templates.pm
+++ lib/PGXN/Site/Templates.pm
@@ -14,7 +14,7 @@ use File::Basename qw(basename);
 use SemVer;
 use Gravatar::URL;
 #use namespace::autoclean; # Do not use; breaks sort {}
-our $VERSION = v0.21.1;
+our $VERSION = v0.22.0;
 
 my $l = PGXN::Site::Locale->get_handle('en');
 sub T { $l->maketext(@_) }
